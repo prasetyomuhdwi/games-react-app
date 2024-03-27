97e334065c0daac90fc9aa8e1868bb8cf148f323
prasetyomuhdwi <muhammad.2365@students.amikom.ac.id>
2024-03-27 10:07:58 +0800

    useState to select menu

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent } from 'react'
+import { useState } from 'react'
 
 function ListGroup() {
   let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris']
@@ -7,23 +7,26 @@ function ListGroup() {
     return items.length === 0 && <p>item found</p>
   }
 
+  const [selectedlndex, setSelectedIndex] = useState(-1)
+
   return (
     // fragment
     <>
       <h1 className="p-4 mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
         Test
       </h1>
+
       <ul className="w-56 menu bg-base-200 rounded-box">
         {getMessage()}
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
             className="p-2"
             key={item}
             onClick={() => {
-              console.log(item)
+              setSelectedIndex(index)
             }}
           >
-            {item}
+            <a className={selectedlndex === index ? 'active' : ''}>{item}</a>
           </li>
         ))}
       </ul>
